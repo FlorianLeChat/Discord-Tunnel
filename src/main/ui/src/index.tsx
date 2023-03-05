9ca5b3dd118477a5003c05a7b07f8cb87b5a5cf5
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2023-03-05 17:33:40 +0100

    Transformation des fichiers en TypeScript et SASS

--- src/main/ui/src/index.tsx
+++ src/main/ui/src/index.tsx
@@ -1,12 +1,12 @@
 // Importation de la feuille de style CSS.
-import "./index.css";
+import "./index.scss";
 
 // Importation de React et de ses dépendances.
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 // Génération de la page.
-const root = createRoot( document.querySelector( "body > main" ) );
+const root = createRoot( document.querySelector( "body > main" ) as Element );
 root.render(
 	<StrictMode>
 		<h1>Ceci est un test</h1>
