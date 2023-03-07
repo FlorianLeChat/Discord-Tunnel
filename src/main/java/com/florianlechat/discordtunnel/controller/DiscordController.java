47d7d6dcfd3fd0d27db574c7842978f44377a769
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2023-03-07 10:32:01 +0100

    Correction de la route API de test côté Spring Boot

--- src/main/java/com/florianlechat/discordtunnel/controller/DiscordController.java
+++ src/main/java/com/florianlechat/discordtunnel/controller/DiscordController.java
@@ -1,33 +1,37 @@
 package com.florianlechat.discordtunnel.controller;
 
-import java.io.IOException;
 import java.net.URL;
+import java.io.IOException;
 import java.net.HttpURLConnection;
-
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/")
 public class DiscordController
 {
+	// Permet d'effectuer une requête HTTP vers l'API de Discord
+	//	afin de simuler le fait qu'un utilisateur est en train de
+	//	taper un message.
 	@GetMapping(path = "/api/typing")
-	public ResponseEntity<String> MakeHttpRequest() throws IOException
+	public ResponseEntity<String> MakeHttpRequest(HttpServletRequest request) throws IOException
 	{
+		// On vérifie d'abord si le mot de passe est correct.
+		String password = request.getParameter("secret");
+
+		if (password == null || password.isEmpty() || !password.equals("laurine"))
+		{
+			return ResponseEntity.status(401).build();
+		}
+
+		// On effectue ensuite la requête HTTP.
 		URL url = new URL("https://www.google.com");
 		HttpURLConnection connection = (HttpURLConnection) url.openConnection();
 
 		connection.setRequestMethod("GET");
 
-		int responseCode = connection.getResponseCode();
-
-		if (responseCode == HttpURLConnection.HTTP_OK)
-		{
-			return ResponseEntity.ok("GET request worked.");
-		}
-
-		return ResponseEntity.status(500).body("GET request failed.");
+		// On retourne enfin le résultat de la requête.
+		return ResponseEntity.status(connection.getResponseCode()).build();
 	}
 }
