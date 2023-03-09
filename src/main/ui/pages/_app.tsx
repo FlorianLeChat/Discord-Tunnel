56d4cb30238673dac128812f2ef66c27f38fe173
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2023-03-09 20:46:34 +0100

    Ajout du fichier manifeste du site

--- src/main/ui/pages/_app.tsx
+++ src/main/ui/pages/_app.tsx
@@ -8,12 +8,16 @@ import "./index.scss";
 
 // Importation des dépendances.
 import Head from "next/head";
+import { useRouter } from "next/router";
 
 // Importation des types.
 import type { AppProps } from "next/app";
 
 export default function DiscordTunnel( { Component, pageProps }: AppProps )
 {
+	// Déclaration des constantes.
+	const { basePath } = useRouter();
+
 	// Génération de la structure de la page.
 	return (
 		<>
@@ -25,14 +29,15 @@ export default function DiscordTunnel( { Component, pageProps }: AppProps )
 				{/* Titre du document */}
 				<title>Discord Tunnel</title>
 
-				{/* Icônes du document */}
-				<link rel="icon" type="image/webp" sizes="16x16" href="assets/favicons/16x16.webp" />
-				<link rel="icon" type="image/webp" sizes="32x32" href="assets/favicons/32x32.webp" />
-				<link rel="icon" type="image/webp" sizes="48x48" href="assets/favicons/48x48.webp" />
-				<link rel="icon" type="image/webp" sizes="192x192" href="assets/favicons/192x192.webp" />
-				<link rel="icon" type="image/webp" sizes="512x512" href="assets/favicons/512x512.webp" />
+				{/* Icônes et manifeste du document */}
+				<link rel="icon" type="image/webp" sizes="16x16" href={basePath + "/assets/favicons/16x16.webp"} />
+				<link rel="icon" type="image/webp" sizes="32x32" href={basePath + "/assets/favicons/32x32.webp"} />
+				<link rel="icon" type="image/webp" sizes="48x48" href={basePath + "/assets/favicons/48x48.webp"} />
+				<link rel="icon" type="image/webp" sizes="192x192" href={basePath + "/assets/favicons/192x192.webp"} />
+				<link rel="icon" type="image/webp" sizes="512x512" href={basePath + "/assets/favicons/512x512.webp"} />
 
-				<link rel="apple-touch-icon" href="assets/favicons/180x180.webp" />
+				<link rel="apple-touch-icon" href={basePath + "/assets/favicons/180x180.webp"} />
+				<link rel="manifest" href={basePath + "/manifest.json"} />
 			</Head>
 
 			{/* Affichage du composant demandé */}
