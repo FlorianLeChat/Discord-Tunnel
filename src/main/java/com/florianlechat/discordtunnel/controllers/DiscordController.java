42bbdac3385b039e60aef83363f3415e89e84ff3
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2023-04-19 21:03:06 +0200

    Ajout de la simulation d'envoi des messages

--- src/main/java/com/florianlechat/discordtunnel/controllers/DiscordController.java
+++ src/main/java/com/florianlechat/discordtunnel/controllers/DiscordController.java
@@ -53,4 +53,49 @@ public class DiscordController
 		// On retourne enfin le résultat de la requête.
 		return ResponseEntity.status(connection.getResponseCode()).build();
 	}
+
+	// Permet d'effectuer une requête HTTP vers l'API de Discord
+	//	afin de simuler le fait qu'un utilisateur envoie un message
+	//  dans un salon.
+	@GetMapping(path = "/api/message")
+	public ResponseEntity<String> SendMessage(HttpServletRequest request) throws IOException
+	{
+		// On vérifie d'abord si le mot de passe est correct.
+		String password = request.getParameter("secret");
+
+		if (password == null || password.isEmpty() || !password.equals("laurine"))
+		{
+			return ResponseEntity.status(401).build();
+		}
+
+		// On effectue ensuite la requête HTTP.
+		URL url = new URL("https://discord.com/api/v9/channels/1097906775291859027/typing");
+		HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+		connection.setRequestMethod("POST");
+
+		connection.setRequestProperty("accept", "*/*");
+		connection.setRequestProperty("accept-language", "fr,fr-FR;q=0.9,en-US;q=0.8");
+		connection.setRequestProperty("authorization", "MTA5NzkwNTA4ODAxOTg0NTI2MA.Gn2uds.jHJQ_WN2WIF7fOOuobeVVb1eAIa2Ut29GQpA-w");
+		connection.setRequestProperty("sec-fetch-dest", "empty");
+		connection.setRequestProperty("sec-fetch-mode", "cors");
+		connection.setRequestProperty("sec-fetch-site", "same-origin");
+		connection.setRequestProperty("x-debug-options", "bugReporterEnabled");
+		connection.setRequestProperty("x-discord-locale", "fr");
+		connection.setRequestProperty("x-super-properties", "eyJvcyI6Ik1hYyBPUyBYIiwiYnJvd3NlciI6IkNocm9tZSIsImRldmljZSI6IiIsInN5c3RlbV9sb2NhbGUiOiJmci1GUiIsImJyb3dzZXJfdXNlcl9hZ2VudCI6Ik1vemlsbGEvNS4wIChNYWNpbnRvc2g7IEludGVsIE1hYyBPUyBYIDEwXzE1XzcpIEFwcGxlV2ViS2l0LzUzNy4zNiAoS0hUTUwsIGxpa2UgR2Vja28pIENocm9tZS8xMTIuMC4wLjAgU2FmYXJpLzUzNy4zNiIsImJyb3dzZXJfdmVyc2lvbiI6IjExMi4wLjAuMCIsIm9zX3ZlcnNpb24iOiIxMC4xNS43IiwicmVmZXJyZXIiOiIiLCJyZWZlcnJpbmdfZG9tYWluIjoiIiwicmVmZXJyZXJfY3VycmVudCI6IiIsInJlZmVycmluZ19kb21haW5fY3VycmVudCI6IiIsInJlbGVhc2VfY2hhbm5lbCI6InN0YWJsZSIsImNsaWVudF9idWlsZF9udW1iZXIiOjE5MDUxMSwiY2xpZW50X2V2ZW50X3NvdXJjZSI6bnVsbCwiZGVzaWduX2lkIjowfQ==");
+		connection.setRequestProperty("referrer", "https://discord.com/channels/@me/1097906775291859027");
+		connection.setRequestProperty("referrerPolicy", "strict-origin-when-cross-origin");
+		connection.setRequestProperty("credentials", "include");
+		connection.setRequestProperty("content-length", "0");
+		connection.setRequestProperty("content-type", "application/json");
+		connection.setRequestProperty("Body", "{\"content\":\"" + request.getParameter("message") + "\",\"nonce\":\"1098317268686733312\",\"tts\":false,\"flags\":0}");
+
+		connection.setDoInput(true);
+		connection.setDoOutput(true);
+
+		connection.connect();
+
+		// On retourne enfin le résultat de la requête.
+		return ResponseEntity.status(connection.getResponseCode()).build();
+	}
 }
