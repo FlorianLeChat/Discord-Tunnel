c30b9b5e78aa40988581abfe82ca2e7eb2dfbe34
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2023-04-19 21:39:59 +0200

    Correction de la définition de l'en-tête `Content-Length`

--- src/main/java/com/florianlechat/discordtunnel/controllers/DiscordController.java
+++ src/main/java/com/florianlechat/discordtunnel/controllers/DiscordController.java
@@ -32,8 +32,13 @@ public class DiscordController
 		connection.setRequestMethod("POST");
 
 		connection.setRequestProperty("accept", "*/*");
-		connection.setRequestProperty("accept-language", "fr,fr-FR;q=0.9,en-US;q=0.8");
+		connection.setRequestProperty("accept-language", "fr-FR,fr;q=0.9");
 		connection.setRequestProperty("authorization", "MTA5NzkwNTA4ODAxOTg0NTI2MA.Gn2uds.jHJQ_WN2WIF7fOOuobeVVb1eAIa2Ut29GQpA-w");
+		connection.setRequestProperty("cache-control", "no-cache");
+		connection.setRequestProperty("pragma", "no-cache");
+		connection.setRequestProperty("sec-ch-ua", "\"Chromium\";v=\"112\", \"Google Chrome\";v=\"112\", \"Not:A-Brand\";v=\"99\"");
+		connection.setRequestProperty("sec-ch-ua-mobile", "?0");
+		connection.setRequestProperty("sec-ch-ua-platform", "\"macOS\"");
 		connection.setRequestProperty("sec-fetch-dest", "empty");
 		connection.setRequestProperty("sec-fetch-mode", "cors");
 		connection.setRequestProperty("sec-fetch-site", "same-origin");
@@ -42,8 +47,10 @@ public class DiscordController
 		connection.setRequestProperty("x-super-properties", "eyJvcyI6Ik1hYyBPUyBYIiwiYnJvd3NlciI6IkNocm9tZSIsImRldmljZSI6IiIsInN5c3RlbV9sb2NhbGUiOiJmci1GUiIsImJyb3dzZXJfdXNlcl9hZ2VudCI6Ik1vemlsbGEvNS4wIChNYWNpbnRvc2g7IEludGVsIE1hYyBPUyBYIDEwXzE1XzcpIEFwcGxlV2ViS2l0LzUzNy4zNiAoS0hUTUwsIGxpa2UgR2Vja28pIENocm9tZS8xMTIuMC4wLjAgU2FmYXJpLzUzNy4zNiIsImJyb3dzZXJfdmVyc2lvbiI6IjExMi4wLjAuMCIsIm9zX3ZlcnNpb24iOiIxMC4xNS43IiwicmVmZXJyZXIiOiIiLCJyZWZlcnJpbmdfZG9tYWluIjoiIiwicmVmZXJyZXJfY3VycmVudCI6IiIsInJlZmVycmluZ19kb21haW5fY3VycmVudCI6IiIsInJlbGVhc2VfY2hhbm5lbCI6InN0YWJsZSIsImNsaWVudF9idWlsZF9udW1iZXIiOjE5MDUxMSwiY2xpZW50X2V2ZW50X3NvdXJjZSI6bnVsbCwiZGVzaWduX2lkIjowfQ==");
 		connection.setRequestProperty("referrer", "https://discord.com/channels/@me/1097906775291859027");
 		connection.setRequestProperty("referrerPolicy", "strict-origin-when-cross-origin");
-		connection.setRequestProperty("Credentials", "include");
-		connection.setRequestProperty("Content-Length", "0");
+		connection.setRequestProperty("body", null);
+		connection.setRequestProperty("mode", "cors");
+		connection.setRequestProperty("credentials", "include");
+		connection.setFixedLengthStreamingMode(0);
 
 		connection.setDoInput(true);
 		connection.setDoOutput(true);
