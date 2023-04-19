5ae584e892fa4e80fc8ca98d0e06ca80ec2c3324
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2023-04-19 21:02:08 +0200

    Mise à jour de la simulation en écriture

--- src/main/java/com/florianlechat/discordtunnel/controllers/DiscordController.java
+++ src/main/java/com/florianlechat/discordtunnel/controllers/DiscordController.java
@@ -15,7 +15,7 @@ public class DiscordController
 	//	afin de simuler le fait qu'un utilisateur est en train de
 	//	taper un message.
 	@GetMapping(path = "/api/typing")
-	public ResponseEntity<String> MakeHttpRequest(HttpServletRequest request) throws IOException
+	public ResponseEntity<String> SendTyping(HttpServletRequest request) throws IOException
 	{
 		// On vérifie d'abord si le mot de passe est correct.
 		String password = request.getParameter("secret");
@@ -26,21 +26,21 @@ public class DiscordController
 		}
 
 		// On effectue ensuite la requête HTTP.
-		URL url = new URL("https://discord.com/api/v9/channels/771427562484138025/typing");
+		URL url = new URL("https://discord.com/api/v9/channels/1097906775291859027/typing");
 		HttpURLConnection connection = (HttpURLConnection) url.openConnection();
 
 		connection.setRequestMethod("POST");
 
 		connection.setRequestProperty("accept", "*/*");
 		connection.setRequestProperty("accept-language", "fr,fr-FR;q=0.9,en-US;q=0.8");
-		connection.setRequestProperty("authorization", "MTgzMjcyNDExMTY3MzI2MjA5.Gbllme.mppZHM4ZllEWVg1cWgzTzEyU1hTeFZNNXZkQTF2VURBZGQ5ZTljNzV3YzRKMWhXaHd3NFNzZUpRcTJuajYtR3BLNlVueFd1a2JQYWRvT0d2");
+		connection.setRequestProperty("authorization", "MTA5NzkwNTA4ODAxOTg0NTI2MA.Gn2uds.jHJQ_WN2WIF7fOOuobeVVb1eAIa2Ut29GQpA-w");
 		connection.setRequestProperty("sec-fetch-dest", "empty");
 		connection.setRequestProperty("sec-fetch-mode", "cors");
 		connection.setRequestProperty("sec-fetch-site", "same-origin");
 		connection.setRequestProperty("x-debug-options", "bugReporterEnabled");
 		connection.setRequestProperty("x-discord-locale", "fr");
-		connection.setRequestProperty("x-super-properties", "eyJvcyI6IldpbmRvd3MiLCJicm93c2VyIjoiRGlzY29yZCBDbGllbnQiLCJyZWxlYXNlX2NoYW5uZWwiOiJzdGFibGUiLCJjbGllbnRfdmVyc2lvbiI6IjEuMC45MDExIiwib3NfdmVyc2lvbiI6IjEwLjAuMTkwNDUiLCJvc19hcmNoIjoieDY0Iiwic3lzdGVtX2xvY2FsZSI6ImZyIiwiY2xpZW50X2J1aWxkX251bWJlciI6MTc4NTkwLCJuYXRpdmVfYnVpbGRfbnVtYmVyIjoyOTU4NCwiY2xpZW50X2V2ZW50X3NvdXJjZSI6bnVsbCwiZGVzaWduX2lkIjowfQ==");
-		connection.setRequestProperty("referrer", "https://discord.com/channels/401777889449607178/771427562484138025");
+		connection.setRequestProperty("x-super-properties", "eyJvcyI6Ik1hYyBPUyBYIiwiYnJvd3NlciI6IkNocm9tZSIsImRldmljZSI6IiIsInN5c3RlbV9sb2NhbGUiOiJmci1GUiIsImJyb3dzZXJfdXNlcl9hZ2VudCI6Ik1vemlsbGEvNS4wIChNYWNpbnRvc2g7IEludGVsIE1hYyBPUyBYIDEwXzE1XzcpIEFwcGxlV2ViS2l0LzUzNy4zNiAoS0hUTUwsIGxpa2UgR2Vja28pIENocm9tZS8xMTIuMC4wLjAgU2FmYXJpLzUzNy4zNiIsImJyb3dzZXJfdmVyc2lvbiI6IjExMi4wLjAuMCIsIm9zX3ZlcnNpb24iOiIxMC4xNS43IiwicmVmZXJyZXIiOiIiLCJyZWZlcnJpbmdfZG9tYWluIjoiIiwicmVmZXJyZXJfY3VycmVudCI6IiIsInJlZmVycmluZ19kb21haW5fY3VycmVudCI6IiIsInJlbGVhc2VfY2hhbm5lbCI6InN0YWJsZSIsImNsaWVudF9idWlsZF9udW1iZXIiOjE5MDUxMSwiY2xpZW50X2V2ZW50X3NvdXJjZSI6bnVsbCwiZGVzaWduX2lkIjowfQ==");
+		connection.setRequestProperty("referrer", "https://discord.com/channels/@me/1097906775291859027");
 		connection.setRequestProperty("referrerPolicy", "strict-origin-when-cross-origin");
 		connection.setRequestProperty("Credentials", "include");
 		connection.setRequestProperty("Content-Length", "0");
