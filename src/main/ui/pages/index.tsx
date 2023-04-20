8836b001aee1e21af54b7078faf10523891d4691
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2023-04-20 10:53:14 +0200

    Ajout des contrôles de présence côté front

--- src/main/ui/pages/index.tsx
+++ src/main/ui/pages/index.tsx
@@ -10,6 +10,7 @@ export default function Home()
 	const [ message, setMessage ] = useState( "" );
 	const [ password, setPassword ] = useState( "" );
 	const [ typingTimer, setTypingTimer ] = useState<NodeJS.Timer>();
+	const [ presenceTimer, setPresenceTimer ] = useState<NodeJS.Timer>();
 
 	const updatePassword = ( event: React.ChangeEvent<HTMLInputElement> ) =>
 	{
@@ -48,6 +49,33 @@ export default function Home()
 		}
 	};
 
+	// Permet de démarrer la simulation de présence.
+	const startPresence = () =>
+	{
+		const presence = () =>
+		{
+			fetch( window.location.pathname + "api/presence?secret=" + password )
+				.then( response => setCode( response.status ) );
+
+			console.log( "Requête de simulation de présence envoyée." );
+		};
+
+		presence();
+
+		setPresenceTimer( setInterval( presence, 60000 ) );
+	};
+
+	// Permet d'arrêter la simulation de présence.
+	const stopPresence = () =>
+	{
+		if ( presenceTimer )
+		{
+			clearInterval( presenceTimer );
+
+			console.log( "Requête de simulation de présence arrêtée." );
+		}
+	};
+
 	// Permet d'envoyer un message au travers de l'API de Discord.
 	const sendMessage = () =>
 	{
@@ -71,6 +99,12 @@ export default function Home()
 			<button onClick={sendMessage}>Appuyez ici pour envoyer le message</button>
 
 			<input type="text" value={message} onChange={updateMessage} placeholder="Message" />
+
+			<h1>Activité en ligne</h1>
+
+			<button onClick={startPresence}>Appuyez ici pour simuler l'écriture</button>
+
+			<button onClick={stopPresence}>Appuyez ici pour arrêter la simulation</button>
 
 			<h2>Sécurité</h2>
 
