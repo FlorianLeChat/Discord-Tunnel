47835015cde3056b0e7e27eeef6f0dbc4fb97e88
Florian Trayon <26360935+FlorianLeChat@users.noreply.github.com>
2023-04-22 14:22:26 +0200

    Amélioration de la récupération des sockets déjà créés

--- src/main/java/com/florianlechat/discordtunnel/controllers/DiscordController.java
+++ src/main/java/com/florianlechat/discordtunnel/controllers/DiscordController.java
@@ -1,12 +1,15 @@
 package com.florianlechat.discordtunnel.controllers;
 
-import java.net.URL;
-import java.util.Base64;
 import java.io.IOException;
 import java.io.OutputStream;
-import java.security.SecureRandom;
+
+import java.net.URL;
 import java.net.HttpURLConnection;
+
+import java.util.Base64;
+import java.security.SecureRandom;
 import jakarta.servlet.http.HttpServletRequest;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,29 +17,32 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DiscordController
 {
+	// Initialisation du WebSocket de Discord.
+	DiscordGateway gateway = new DiscordGateway();
+
 	// Permet de générer un nombre arbitraire en Base64.
-    public static String generateNonce()
+	public static String generateNonce()
 	{
 		// On définit la longueur du nombre arbitraire
 		//  avant de créer un tableau de bytes pour le stocker.
-        int nonceLength = 19;
-        byte[] nonceBytes = new byte[nonceLength];
+		int nonceLength = 19;
+		byte[] nonceBytes = new byte[nonceLength];
 
 		// On utilise ensuite SecureRandom pour générer des
 		//  bytes aléatoires.
-        SecureRandom secureRandom = new SecureRandom();
-        secureRandom.nextBytes(nonceBytes);
+		SecureRandom secureRandom = new SecureRandom();
+		secureRandom.nextBytes(nonceBytes);
 
 		// On encode ensuite ces bytes en Base64 pour obtenir
 		//  une chaîne de caractères.
-        String nonce = Base64.getUrlEncoder().withoutPadding().encodeToString(nonceBytes);
+		String nonce = Base64.getUrlEncoder().withoutPadding().encodeToString(nonceBytes);
 
 		// On tronque enfin la chaîne de caractères pour
 		//  obtenir la longueur souhaitée.
-        nonce = nonce.substring(0, nonceLength);
+		nonce = nonce.substring(0, nonceLength);
 
-        return nonce;
-    }
+		return nonce;
+	}
 
 	// Permet d'effectuer une requête HTTP vers l'API de Discord
 	//  afin de simuler le fait qu'un utilisateur est en train de
@@ -159,46 +165,25 @@ public class DiscordController
 			return ResponseEntity.status(401).build();
 		}
 
-		// On effectue ensuite la requête HTTP.
-		URL url = new URL("https://discord.com/api/v9/science");
-		long epoch = System.currentTimeMillis();
-		String body = "{\"token\":\"MTA5NzkwNTA4ODAxOTg0NTI2MA.tTzqjz544OwGVb7m-ITFBqn2_fY\",\"events\":[{\"type\":\"client_heartbeat\",\"properties\":{\"client_track_timestamp\":" + epoch + ",\"client_heartbeat_session_id\":\"f184b204-8f95-425b-a430-4b4d0b843176\",\"client_heartbeat_initialization_timestamp\":1681976469887,\"client_heartbeat_version\":7,\"client_performance_memory\":0,\"accessibility_features\":524544,\"rendered_locale\":\"fr\",\"accessibility_support_enabled\":false,\"client_uuid\":\"jFCEUNyKPA8tUHCe69ScnYcBAAAFAAAA\",\"client_send_timestamp\":" + epoch + "}}]}";
-		HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+		// On vérifie ensuite si un état a été spécifié.
+		String state = request.getParameter("state");
 
-		connection.setRequestMethod("POST");
+		if (state == null || state.isEmpty())
+		{
+			return ResponseEntity.status(400).build();
+		}
 
-		connection.setRequestProperty("accept", "*/*");
-		connection.setRequestProperty("accept-language", "fr,fr-FR;q=0.9,en-US;q=0.8");
-		connection.setRequestProperty("authorization", "MTA5NzkwNTA4ODAxOTg0NTI2MA.GJIDh4.-C6QaRjfJ0yigjAAuC1XCdM9ThmcESjhbOr358");
-		connection.setRequestProperty("cache-control", "no-cache");
-		connection.setRequestProperty("content-type", "application/json");
-		connection.setRequestProperty("sec-ch-ua", "\"Chromium\";v=\"112\", \"Google Chrome\";v=\"112\", \"Not:A-Brand\";v=\"99\"");
-		connection.setRequestProperty("sec-ch-ua-mobile", "?0");
-		connection.setRequestProperty("sec-ch-ua-platform", "\"macOS\"");
-		connection.setRequestProperty("sec-fetch-dest", "empty");
-		connection.setRequestProperty("sec-fetch-mode", "cors");
-		connection.setRequestProperty("sec-fetch-site", "same-origin");
-		connection.setRequestProperty("x-debug-options", "bugReporterEnabled");
-		connection.setRequestProperty("x-discord-locale", "fr");
-		connection.setRequestProperty("x-super-properties", "eyJvcyI6Ik1hYyBPUyBYIiwiYnJvd3NlciI6IkNocm9tZSIsImRldmljZSI6IiIsInN5c3RlbV9sb2NhbGUiOiJmci1GUiIsImJyb3dzZXJfdXNlcl9hZ2VudCI6Ik1vemlsbGEvNS4wIChNYWNpbnRvc2g7IEludGVsIE1hYyBPUyBYIDEwXzE1XzcpIEFwcGxlV2ViS2l0LzUzNy4zNiAoS0hUTUwsIGxpa2UgR2Vja28pIENocm9tZS8xMTIuMC4wLjAgU2FmYXJpLzUzNy4zNiIsImJyb3dzZXJfdmVyc2lvbiI6IjExMi4wLjAuMCIsIm9zX3ZlcnNpb24iOiIxMC4xNS43IiwicmVmZXJyZXIiOiIiLCJyZWZlcnJpbmdfZG9tYWluIjoiIiwicmVmZXJyZXJfY3VycmVudCI6IiIsInJlZmVycmluZ19kb21haW5fY3VycmVudCI6IiIsInJlbGVhc2VfY2hhbm5lbCI6InN0YWJsZSIsImNsaWVudF9idWlsZF9udW1iZXIiOjE5MDUxMSwiY2xpZW50X2V2ZW50X3NvdXJjZSI6bnVsbCwiZGVzaWduX2lkIjowfQ==");
-		connection.setRequestProperty("referrer", "https://discord.com/channels/@me/1097906775291859027");
-		connection.setRequestProperty("referrerPolicy", "strict-origin-when-cross-origin");
-		connection.setRequestProperty("mode", "cors");
-		connection.setRequestProperty("credentials", "include");
-		connection.setFixedLengthStreamingMode(body.getBytes("UTF-8").length);
+		// On effectue alors la liaison aux WebSockets de Discord.
+		if (state.equals("true"))
+		{
+			gateway.connect("MTA5NzkwNTA4ODAxOTg0NTI2MA.GJIDh4.-C6QaRjfJ0yigjAAuC1XCdM9ThmcESjhbOr358");
+		}
+		else
+		{
+			gateway.close();
+		}
 
-		connection.setDoInput(true);
-		connection.setDoOutput(true);
-
-		// On écrit alors le corps de la requête avant
-		//  de l'envoyer.
-		OutputStream os = connection.getOutputStream();
-		os.write(body.getBytes("UTF-8"));
-		os.close();
-
-		connection.connect();
-
-		// On retourne enfin le résultat de la requête.
-		return ResponseEntity.status(connection.getResponseCode()).build();
+		// On retourne enfin le résultat de la liaison.
+		return ResponseEntity.status(200).build();
 	}
 }
